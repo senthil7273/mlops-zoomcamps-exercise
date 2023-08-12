36ccd48fc96295bca458fa49340e066a05f303b4
senthil <senthil7273@gmail.com>
2023-08-12 23:27:50 +0100

    Update main.tf

--- src/infra/modules/featurestore/main.tf
+++ src/infra/modules/featurestore/main.tf
@@ -1,5 +1,4 @@
 resource "google_vertex_ai_featurestore" "featurestore" {
-  description = "creates a feature store named cencer"
   name     = "cancer"
   region = var.region
   force_destroy = true
@@ -10,7 +9,6 @@ resource "google_vertex_ai_featurestore" "featurestore" {
 }
 
 resource "google_vertex_ai_featurestore_entitytype" "entity" {
-  description = "counties in US are the entities in this feature store "
   name     = var.entity_type
   featurestore = google_vertex_ai_featurestore.featurestore.id
 }
