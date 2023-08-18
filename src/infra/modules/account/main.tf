4c8788eb9cb323f44da8c526682e8c068a649954
senthil <senthil7273@gmail.com>
2023-08-18 21:57:12 +0100

    removed duplicate names

--- src/infra/modules/account/main.tf
+++ src/infra/modules/account/main.tf
@@ -3,22 +3,22 @@ resource "google_service_account" "mlops-sa" {
     display_name = "mlops-service-account"
 }
 
-resource "google_project_iam_binding" "project" {
+resource "google_project_iam_binding" "ai-user" {
   role = "aiplatform.user"
   members = [google_service_account.mlops-sa]
 }
 
-resource "google_project_iam_binding" "project" {
+resource "google_project_iam_binding" "s3-view-file" {
   role = "storage.objectViewer"
   members = [google_service_account.mlops-sa]
 }
 
-resource "google_project_iam_binding" "project" {
+resource "google_project_iam_binding" "s3-create-file" {
   role = "storage.objectCreator"
   members = [google_service_account.mlops-sa]
 }
 
-resource "google_project_iam_binding" "project" {
+resource "google_project_iam_binding" "s3-admin" {
   role = "storage.objectAdmin"
   members = [google_service_account.mlops-sa]
 }
