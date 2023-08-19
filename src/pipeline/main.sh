cc79e69d07c4b1cd5bffeb36a805c44dd1fe05c4
senthil <senthil7273@gmail.com>
2023-08-20 00:16:24 +0100

    enabled bash

---
+++ src/pipeline/main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "** setting environment variables **"
+PROJECT_ID=$(gcloud config get-value project)
+BUCKET="ml-staging-mlzoomcamp"
+REGION="europe-west1"
+PIPELINES_HOST=$1
+
+echo "** initiating cloud function **"
+    gcloud functions deploy init_pipeline --runtime python39 \
+     --set-env-vars PROJECT=${PROJECT_ID},BUCKET=${BUCKET}, \
+     --trigger-resource="${BUCKET}"  \
+     --trigger-event=google.storage.object.finalize
